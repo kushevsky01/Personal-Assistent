b30a936aaa409449422db4cad0180ffba6627dfa
kushevsky01 <kushevskij01@mail.ru>
2022-12-17 22:56:57 +0300

    Delete src directory

--- src/main/java/web/note/perository/AffairsRepository.java
+++
@@ -1,8 +0,0 @@
-package web.note.perository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.note.model.Affairs;
-
-public interface AffairsRepository extends JpaRepository<Affairs, Long> {
-
-}
--- src/test/java/web/web_note/WebNoteApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package web.web_note;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebNoteApplicationTests {
-
-}
